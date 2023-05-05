dfab6bc319a74b7c2e9ce5193f3d755cd7d3cdc9
asmitaneupane <neupane.ashmita2056@gmail.com>
2023-05-05 14:51:12 +0545

    social media app on react with firebase

---
+++ src/components/Login.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
